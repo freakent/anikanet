c122aafe9b0d7df7ee1233d37dd1315a437ac1e5
Martin Jarvis <martin@tango.lan>
2020-03-08 13:30:37 +0000

    Added notes to README and gitssh so I don't forget

--- gitssh.sh
+++ gitssh.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+# See https://forum.archive.openwrt.org/viewtopic.php?id=47551
+# Generate your identity key on openwrt
+# dropbearkey -t rsa -f ~/.ssh/id_rsa
+# Copy this file to your home directory and rename to a hidden file
+# cp gitssh.sh ~/gitssh.sh
+# chmod +x ~/.gitssh.sh
+# echo "export GIT_SSH=\$HOME/.gitssh.sh" >> /etc/profile
+
 dbclient -y -i ~/.ssh/id_rsa $*
