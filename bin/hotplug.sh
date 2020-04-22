f4d70c2feb0b22dedb4fb7f0d8eabac875888048
Martin Jarvis <martin@tango.lan>
2020-04-22 22:36:49 +0100

    Logging the active count

--- bin/hotplug.sh
+++ bin/hotplug.sh
@@ -5,11 +5,12 @@ echo `date`, $ACTION, $INTERFACE, $DEVICE >> $LOGFILE
 case "$INTERFACE" in (wan|wwan|tethering|modem)
     case "$ACTION" in (ifup|ifdown)
         active=$(mwan3 interfaces | grep 0 | wc -l)
+
         if [ $active -eq 0 ]
         then
-            echo "guest wifi off" >> $LOGFILE
+            echo "guest wifi off ($active)" >> $LOGFILE
         else
-            echo "guest wifi on" >> $LOGFILE
+            echo "guest wifi on ($active)" >> $LOGFILE
         fi 
     ;; esac
 ;;esac
