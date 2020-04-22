4b4ab424718fba8542f7c7db70a09393c8d2d3e5
Martin Jarvis <martin@tango.lan>
2020-04-22 22:24:13 +0100

    updated hotplug script

--- bin/hotplug.sh
+++ bin/hotplug.sh
@@ -1,3 +1,15 @@
 # ln -s /opt/anikanet/bin/hotplug.sh /etc/hotplug.d/iface/39-anikanet
 LOGFILE=/tmp/anikanet.log
 echo `date`, $ACTION, $INTERFACE, $DEVICE >> $LOGFILE
+
+case "$INTERFACE" in (wan|wwan|tethering|modem)
+    case "$ACTION" in (ifup|ifdown)
+        active=$(mwan3 interfaces | grep 0 | wc -l)
+        if [ $active -eq 0 ]
+        then
+            echo "guest wifi off" >> $LOGFILE
+        else
+            echo "guest wifi on" >> $LOGFILE
+        fi 
+    ;; esac
+;;esac
