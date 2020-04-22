9c58fd6ac4ac560c2e6cf3b4002bf91b8ca2eaa5
Martin Jarvis <martin@tango.local>
2020-04-22 08:38:25 +0100

    Changed to anonymous github url to simplify things drastically

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh 
 
 git clone git@github.com:freakent/anikanet.git /opt/anikanet
+
+git clone git://github.com:freakent/anikanet.git /opt/anikanet
--- setup.sh
+++ setup.sh
@@ -11,24 +11,27 @@ echo "Checking for presence of git"
 (which git) ||  (opkg install git)
 
 #Generate your identity key on openwrt
-echo 
-echo "Checking for private key"
-mkdir -p ~/.ssh
-[ -f ~/.ssh/id_rsa ] || dropbearkey -t rsa -f ~/.ssh/id_rsa
+#echo 
+#echo "Checking for private key"
+#mkdir -p ~/.ssh
+#[ -f ~/.ssh/id_rsa ] || dropbearkey -t rsa -f ~/.ssh/id_rsa
 
 # Change git ssh command
-echo
-echo "Configuring git to use busybox keys"
-echo "#!/bin/sh" > ~/.gitssh.sh
-echo "dbclient -y -i ~/.ssh/id_rsa \$*" >> ~/.gitssh.sh
-chmod +x ~/.gitssh.sh
-echo "export GIT_SSH=\$HOME/.gitssh.sh" >> ~/.profile
+#echo
+#echo "Configuring git to use busybox keys"
+#echo "#!/bin/sh" > ~/.gitssh.sh
+#echo "dbclient -y -i ~/.ssh/id_rsa \$*" >> ~/.gitssh.sh
+#chmod +x ~/.gitssh.sh
+#echo "export GIT_SSH=\$HOME/.gitssh.sh" >> ~/.profile
 
-echo "export GIT_SSH=$HOME/.gitssh.sh"
+#echo "export GIT_SSH=$HOME/.gitssh.sh"
 
 # Last step
 # Convert public key from dropbear binary to openssh text
 # Copy and paste output from below to git account ssh keys
-dropbearkey -y -f ~/.ssh/id_rsa | head -n 2 | tail -1
+#dropbearkey -y -f ~/.ssh/id_rsa | head -n 2 | tail -1
 
 # Now login again to openwrt
+echo
+echo "Clone the anikanet tools into /opt/anikanet"
+git clone git://github.com:/freakent/anikanet.git /opt/anikanet
