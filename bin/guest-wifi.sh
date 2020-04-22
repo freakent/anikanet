33af156c53ba77e102cae1c003f4890afc3787ae
Martin Jarvis <martin@tango.lan>
2020-04-22 23:15:57 +0100

    Fixed calling script

--- bin/guest-wifi.sh
+++ bin/guest-wifi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ANIKANET_HOME=/opt/anikanet
-$ANIKANET_HOME/bin/wifi-ctrl ANIKANET $1
-./wifi-ctrl ANIKANET-5G $1
+$ANIKANET_HOME/bin/wifi-ctrl.sh ANIKANET $1
+$ANIKANET_HOME/bin/wifi-ctrl.sh ANIKANET-5G $1
 
