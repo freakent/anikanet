87e4240122e0a3caefb074c6997e0fed4f35eb78
Martin Jarvis <martin@tango.local>
2020-04-22 16:48:03 +0100

    Fixed typo in name

--- bin/hotplug.sh
+++ bin/hotplug.sh
@@ -1,5 +1,5 @@
 # ln -s /opt/anikanet/bin/hotplug.sh /etc/hotplug.d/iface/39-anikanet
-LOGFILE=/tmp/anikatent.log
+LOGFILE=/tmp/anikanet.log
 date >> $LOGFILE
 env >> $LOGFILE
 echo "***********************" >> $LOGFILE
