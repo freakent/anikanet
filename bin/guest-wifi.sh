9bfd36607447f4459eb78f4db2c9c0a50422ac37
Martin Jarvis <martin@tango.lan>
2020-04-23 00:39:19 +0100

    even more degug

--- bin/guest-wifi.sh
+++ bin/guest-wifi.sh
@@ -1,14 +1,23 @@
-#!/bin/sh
+#!/bin/sh +x
 ANIKANET_HOME=/opt/anikanet
 
 guests=$(uci show wireless | grep guest | grep wifi-iface | cut -d'=' -f1 | cut -d'.' -f2)
 for network in $guests
 do 
   case $1 in
-    off|Off|OFF|0) uci set wireless.${network}.disabled=1 ;;
-    on|On|ON|1)    uci set wireless.${network}.disabled=0 ;;
+    off|Off|OFF|0) 
+        echo "uci set wireless.${network}.disabled=1"
+        uci set wireless.${network}.disabled=1 
+        ;;
+    on|On|ON|1)
+        echo "uci set wireless.${network}.disabled=0"
+        uci set wireless.${network}.disabled=0 
+        ;;
   esac
 done
+
+uci show wireless.guest2g
+uci show wireless.guest5g
 
 uci commit wireless
 #wifi reload $network
