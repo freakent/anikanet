2e8fbb4eba645f6b56b2402a5b5b5516fec2796a
Martin Jarvis <martin@tango.lan>
2020-04-23 00:33:17 +0100

    big change to how guest-wifi works

--- bin/guest-wifi.sh
+++ bin/guest-wifi.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 ANIKANET_HOME=/opt/anikanet
-$ANIKANET_HOME/bin/wifi-ctrl.sh ANIKANET $1
-$ANIKANET_HOME/bin/wifi-ctrl.sh ANIKANET-5G $1
+
+guests=$(uci show wireless | grep guest | grep wifi-iface | cut -d'=' -f1 | cut -d'.' -f2)
+for network in $guests
+do 
+  case $1 in
+    off|Off|OFF|0) uci set wireless.${network}.disabled=1 ;;
+    on|On|ON|1)    uci set wireless.${network}.disabled=0 ;;
+  esac
+done
+
+uci commit wireless
+#wifi reload $network
+wifi reload 
 
