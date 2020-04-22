bd61294a0a531cb72b03f5595cfc3d99383d1316
Martin Jarvis <martin@tango.local>
2020-04-22 17:12:48 +0100

    reformat logfile

--- bin/hotplug.sh
+++ bin/hotplug.sh
@@ -1,5 +1,3 @@
 # ln -s /opt/anikanet/bin/hotplug.sh /etc/hotplug.d/iface/39-anikanet
 LOGFILE=/tmp/anikanet.log
-date >> $LOGFILE
-env >> $LOGFILE
-echo "***********************" >> $LOGFILE
+echo `date`, $ACTION, $INTERFACE, $DEVICE >> $LOGFILE
